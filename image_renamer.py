1242345d1749ea8d1f9cc102fb055f2c550a97ff
AISSI MOHAMMED <aissi.mohammed2014@gmail.com>
2022-12-29 21:44:04 +0100

    Update image_renamer.py

--- image_renamer.py
+++ image_renamer.py
@@ -1,15 +1,14 @@
 import os 
 
-source_folder = r'C:\Users\MSI\Desktop\folder'
+source_folder = r'C:\Users\name_of_user\path_to_folder'
 
 for item in os.listdir(source_folder):
 	if os.path.isfile(os.path.join(source_folder, item)):
 		if item.endswith('.jpeg'):
 			try:
-
 				os.rename(
 					os.path.join(source_folder, item),
-					os.path.join(source_folder, 'FACTURE_'+ item)
+					os.path.join(source_folder, 'name_your_file'+ item)
 					)
 				
 			except PermissionError:
